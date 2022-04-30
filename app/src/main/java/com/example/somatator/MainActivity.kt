ccf131a49bae8e4197a185518db1c059071d6b5d
cleiltonbv <cleiltonsilvabv@gmail.com>
2022-04-29 22:54:38 -0400

    Start commit!

---
+++ app/src/main/java/com/example/somatator/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.somatator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnCalcular.setOnClickListener {
+
+            val peso = edtPeso.text.toString().toDouble()
+            val altura = edtAltura.text.toString().toDouble()
+
+
+            val resultado = peso / (altura * altura)
+
+            tvResultado.text ="Seu IMC é $resultado"
+
+        }
+
+
+    }
+}
